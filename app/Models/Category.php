0d3525e64614a86c21de2a4bcb7e0440549bca7e
Abdul Alim <aa.abdulalim13@gmail.com>
2024-03-10 17:50:38 +0600

    added category model and migration

---
+++ app/Models/Category.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'parent_id', 'slug', 'image', 'description'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($category) {
+            $category->slug = Str::slug($category->name);
+        });
+    }
+}
