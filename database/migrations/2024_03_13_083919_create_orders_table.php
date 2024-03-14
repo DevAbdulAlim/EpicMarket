cdc15feea444af42824e9204c7e4d324e518c5fe
Abdul Alim <aa.abdulalim13@gmail.com>
2024-03-14 15:20:38 +0600

    updated order table

--- database/migrations/2024_03_13_083919_create_orders_table.php
+++ database/migrations/2024_03_13_083919_create_orders_table.php
@@ -14,9 +14,16 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('address_id');
+            $table->string('payment_method');
+            $table->decimal('subtotal', 10, 2);
+            $table->decimal('tax', 10, 2);
+            $table->decimal('shipping', 10, 2);
+            $table->decimal('total', 10, 2);
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('address_id')->references('id')->on('addresses');
         });
     }
 
--- database/migrations/2024_03_13_085118_create_order_items_table.php
+++ database/migrations/2024_03_13_085118_create_order_items_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('order_id');
             $table->unsignedBigInteger('product_id');
+            $table->decimal('price', 10, 2);
             $table->integer('quantity');
             $table->timestamps();
 
