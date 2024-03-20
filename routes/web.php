b21ceb2da76dfbd0d83132737203de962faa2209
Abdul Alim <aa.abdulalim13@gmail.com>
2024-03-20 10:12:41 +0600

    added email verification

--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,39 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Livewire\Cart;
 use App\Livewire\Checkout;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class,'index'])->name('home');
 Route::get('/product/search', [ProductController::class, 'search'])->name('product.search');
 Route::get('/product/{product}', [ProductController::class, 'details'])->name('product.details');
 
-Route::middleware(['auth'])->group(function () {
+
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/cart', Cart::class)->name('cart');
     Route::get('/checkout', Checkout::class)->name('checkout');
     Route::get('/user/orders', [UserController::class, 'orders'])->name('user.orders');
     Route::get('/user/orders/{order}', [UserController::class, 'orderDetails'])->name('user.order.details');
 });
+
+// Email verification
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
 
 Route::middleware([
     'auth:sanctum',
