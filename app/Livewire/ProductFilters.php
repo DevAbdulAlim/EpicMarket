e8e87a6c2e38780cde0c4b7e5d9c7e2b9f902754
Abdul Alim <aa.abdulalim13@gmail.com>
2024-03-21 10:51:48 +0600

    updated product filter

--- app/Livewire/ProductFilters.php
+++ app/Livewire/ProductFilters.php
@@ -8,20 +8,42 @@ use Livewire\Component;
 
 class ProductFilters extends Component
 {
+    public $minPrice = '';
+    public $maxPrice = '';
     public $categories;
     public $selectedCategories = [];
 
-    public function mount(Request $request)
+    public function mount()
     {
+        $request = request();
         $this->categories = Category::take(8)->get();
 
+        // Set minPrice and maxPrice from the query parameters
+        $this->minPrice = $request->query('minPrice');
+        $this->maxPrice = $request->query('maxPrice');
+
+        // Set selectedCategories from the query parameters
         $selectedCategoriesFromUrl = $request->query('categories');
+
+        if ($selectedCategoriesFromUrl && is_array($selectedCategoriesFromUrl)) {
+            // Convert the array to a string
+            $selectedCategoriesFromUrl = implode(',', $selectedCategoriesFromUrl);
+        }
 
         if ($selectedCategoriesFromUrl) {
             $this->selectedCategories = explode(',', $selectedCategoriesFromUrl);
         }
-
     }
+
+
+
+    public function priceFilter()
+    {
+        $queryParams = $this->generateQueryParams();
+
+        $this->redirect(route('product.search', $queryParams));
+    }
+
     public function updateSelectedCategories($value)
     {
         if (in_array($value, $this->selectedCategories)) {
@@ -32,11 +54,30 @@ class ProductFilters extends Component
             $this->selectedCategories[] = $value;
         }
 
-        // Convert the array of selected categories back to a comma-separated string
-        $selectedCategoriesString = implode(',', $this->selectedCategories);
+        // Build query parameters array
+        $queryParams = $this->generateQueryParams();
 
-        // Redirect to the product.search route with the updated selected categories
-        $this->redirect(route('product.search', ['categories' => $selectedCategoriesString]));
+        // Redirect to the product.search route with the updated query parameters
+        $this->redirect(route('product.search', $queryParams));
+    }
+
+    private function generateQueryParams()
+    {
+        $queryParams = [];
+
+        if (!empty ($this->minPrice)) {
+            $queryParams['minPrice'] = $this->minPrice;
+        }
+
+        if (!empty ($this->maxPrice)) {
+            $queryParams['maxPrice'] = $this->maxPrice;
+        }
+
+        if (!empty ($this->selectedCategories)) {
+            $queryParams['categories'] = implode(',', $this->selectedCategories);
+        }
+
+        return $queryParams;
     }
 
 
--- resources/views/livewire/product-filters.blade.php
+++ resources/views/livewire/product-filters.blade.php
@@ -3,12 +3,12 @@
     <Button>Filter</Button>
     <div class="mb-8">
         <h3 class="mb-2 text-gray-600">Filter by Price:</h3>
-        <form action="{{ route('product.search') }}" method="GET">
+        <form wire:submit="priceFilter" method="GET">
             <div class="flex items-center mb-4">
-                <input type="number" name="min_price" id="min_price" placeholder="Min"
+                <input type="number" wire:model="minPrice" id="min_price" min="0" placeholder="Min"
                     class="w-16 p-1 text-sm text-center border rounded-md ">
                 <span class="mx-2">-</span>
-                <input type="number" name="max_price" id="max_price" placeholder="Max"
+                <input type="number" wire:model="maxPrice" id="max_price" min="0" placeholder="Max"
                     class="w-16 p-1 text-sm text-center border rounded-md ">
                 <button type="submit" class="p-1 ml-2 text-white bg-blue-500 rounded-md">Apply</button>
             </div>
