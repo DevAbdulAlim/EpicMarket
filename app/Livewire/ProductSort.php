25043be5120f63341763f85fc19e02716869588f
Abdul Alim <aa.abdulalim13@gmail.com>
2024-03-21 13:32:51 +0600

    fixed product sort

--- app/Livewire/ProductSort.php
+++ app/Livewire/ProductSort.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Livewire;
 
 use Livewire\Component;
@@ -8,33 +7,31 @@ class ProductSort extends Component
 {
     public $total;
     public $sortValue;
-
     public $queryParams;
-
 
     public function mount($total)
     {
         $this->total = $total;
         $this->queryParams = request()->query();
 
+        // Set initial value of $sortValue based on query parameters if they exist
+        $sortBy = isset ($this->queryParams['sortBy']) ? $this->queryParams['sortBy'] : 'price';
+        $sortDirection = isset ($this->queryParams['sortDirection']) ? $this->queryParams['sortDirection'] : 'asc';
+        $this->sortValue = $sortBy . '-' . $sortDirection;
     }
+
     public function handleSort()
     {
         // Extract sorting criteria and direction from the selected value
         list($criteria, $direction) = explode('-', $this->sortValue);
 
         // Set the new sorting criteria and direction
-        $this->sortBy = $criteria;
-        $this->sortDirection = $direction;
-
-        // Add the new sorting criteria and direction to the query parameters
-        $this->queryParams['sortBy'] = $this->sortBy;
-        $this->queryParams['sortDirection'] = $this->sortDirection;
+        $this->queryParams['sortBy'] = $criteria;
+        $this->queryParams['sortDirection'] = $direction;
 
         // Redirect to the current route with the updated query parameters
         $this->redirect(route('product.search', $this->queryParams));
     }
-
 
     public function render()
     {
--- resources/views/livewire/product-sort.blade.php
+++ resources/views/livewire/product-sort.blade.php
@@ -6,7 +6,7 @@
         <select id="sort" name="sort" class="text-sm border rounded-full" wire:model="sortValue"
             wire:change="handleSort">
             <option value="price-asc">Price Low to High</option>
-            <option value="price-desc">Price High to Low</option>
+            <option value="price-desc" selected>Price High to Low</option>
         </select>
     </div>
 </div>
