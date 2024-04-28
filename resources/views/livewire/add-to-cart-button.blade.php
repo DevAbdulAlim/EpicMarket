51028e44e760a0a96c54af4c1165b2e0be56bb20
Abdul Alim <aa.abdulalim13@gmail.com>
2024-04-28 17:12:45 +0600

    updated shopping cart

--- resources/views/livewire/add-to-cart-button.blade.php
+++ resources/views/livewire/add-to-cart-button.blade.php
@@ -1,7 +1,7 @@
 <div>
-    <x-action-message on="click" class="fixed">
+    <x-action-message on="click" class="fixed left-5 top-20 z-50">
         @if ($message)
-            <div class="fixed left-5 top-20 z-50 bg-green-500 text-white px-4 py-2 flex items-center shadow-2xl">
+            <div class=" bg-green-500 text-white px-5 py-2 flex items-center shadow-lg">
                 <i class="fas fa-check-circle mr-2"></i>
                 {{ $message }}
             </div>
--- resources/views/livewire/shopping-cart.blade.php
+++ resources/views/livewire/shopping-cart.blade.php
@@ -1,8 +1,7 @@
-<div>
+<div x-data="{ open: false }">
 
-    <button wire:click="openCart" class="relative">
-        <span
-            class="absolute inline-flex items-center justify-center w-6 h-6 -mt-2 -mr-2 text-white bg-red-500 rounded-full -top-1 -right-1">{{ count($cart) }}</span>
+    <button x-on:click="open = true" class="relative">
+        <span class="absolute inline-flex items-center justify-center w-6 h-6 -mt-2 -mr-2 text-white bg-red-500 rounded-full -top-1 -right-1">{{ count($cart) }}</span>
         <i class="fas fa-shopping-cart"></i>
     </button>
 
@@ -12,54 +11,49 @@
         </div>
     </div>
 
+    <div x-show="open" @click.outside="open=false" class="fixed top-0 right-0 z-50 h-full overflow-y-auto bg-white shadow-lg w-96">
+        <div class="p-4 relative h-full">
+            <div class="flex justify-between pb-2 mb-4 text-xl font-semibold border-b">
+                <h2>Shopping Cart ({{ count($cart) }} items)</h2>
+                <button x-on:click="open = false">X</button>
+            </div>
 
-    @if ($cartOpen)
-        <div class="fixed top-0 right-0 z-50 h-full overflow-y-auto bg-white shadow-lg w-96">
-            <div class="p-4 relative h-full">
-                <div class="flex justify-between pb-2 mb-4 text-xl font-semibold border-b">
-                    <h2>Shopping Cart ({{ count($cart) }} items)</h2>
-                    <button wire:click="closeCart">X</button>
-                </div>
-
-                @if (count($cart) > 0)
-                    <ul>
-                        @foreach ($cart as $productId => $item)
-                            <li class="flex items-center justify-between py-2 border-b">
-                                <div class="flex items-center space-x-2">
-                                    <div class="flex-shrink-0 w-16 h-16 overflow-hidden bg-gray-200 rounded-md">
-                                        <img src="{{asset('/images/product.webp')}}" alt="{{ $item['name'] }}"
-                                            class="object-cover w-full h-full">
-                                    </div>
-                                    <div>
-                                        <p class="text-lg font-semibold">{{ $item['name'] }}</p>
-                                        <p class="text-sm">${{ $item['price'] }} x {{ $item['quantity'] }} =
-                                            ${{ $item['price'] * $item['quantity'] }}</p>
-                                    </div>
+            @if (count($cart) > 0)
+                <ul>
+                    @foreach ($cart as $productId => $item)
+                        <li class="flex items-center justify-between py-2 border-b">
+                            <div class="flex items-center space-x-2">
+                                <div class="flex-shrink-0 w-16 h-16 overflow-hidden bg-gray-200 rounded-md">
+                                    <img src="{{asset('/images/product.webp')}}" alt="{{ $item['name'] }}"
+                                        class="object-cover w-full h-full">
                                 </div>
-                                <div class="flex space-x-2">
-                                    <button wire:click="increaseQuantity({{ $productId }})"
-                                        class="px-3 py-1 text-white bg-green-500 rounded hover:bg-green-600">+</button>
-                                    <button wire:click="decreaseQuantity({{ $productId }})"
-                                        class="px-3 py-1 text-white bg-yellow-500 rounded hover:bg-yellow-600">-</button>
-                                    <button wire:click="removeFromCart({{ $productId }})"
-                                        class="px-3 py-1 text-white bg-red-500 rounded hover:bg-red-600">x</button>
+                                <div>
+                                    <p class="text-lg font-semibold">{{ $item['name'] }}</p>
+                                    <p class="text-sm">${{ $item['price'] }} x {{ $item['quantity'] }} =
+                                        ${{ $item['price'] * $item['quantity'] }}</p>
                                 </div>
-                            </li>
-                        @endforeach
-                    </ul>
-                @else
-                    <p class="text-lg">Your cart is empty.</p>
-                @endif
+                            </div>
+                            <div class="flex space-x-2">
+                                <button wire:click="increaseQuantity({{ $productId }})"
+                                    class="px-3 py-1 text-white bg-green-500 rounded hover:bg-green-600">+</button>
+                                <button wire:click="decreaseQuantity({{ $productId }})"
+                                    class="px-3 py-1 text-white bg-yellow-500 rounded hover:bg-yellow-600">-</button>
+                                <button wire:click="removeFromCart({{ $productId }})"
+                                    class="px-3 py-1 text-white bg-red-500 rounded hover:bg-red-600">x</button>
+                            </div>
+                        </li>
+                    @endforeach
+                </ul>
+            @else
+                <p class="text-lg">Your cart is empty.</p>
+            @endif
 
-                <div class="flex justify-center text-center p-4">
-                    <a href="{{ $totalPrice > 0 ? route('checkout') : '#' }}" class="bg-green-800 left-2 right-2 text-white absolute bottom-2 hover:bg-green-900 font-bold py-2 px-4 rounded">
-                        Checkout{{ $totalPrice > 0 ? ' (Total $'.$totalPrice.')' : '' }}
-                    </a>
-                </div>
-
+            <div class="flex justify-center text-center p-4">
+                <a href="{{ $totalPrice > 0 ? route('checkout') : '#' }}" class="bg-green-800 left-2 right-2 text-white absolute bottom-2 hover:bg-green-900 font-bold py-2 px-4 rounded">
+                    Checkout{{ $totalPrice > 0 ? ' (Total $'.$totalPrice.')' : '' }}
+                </a>
             </div>
         </div>
-    @endif
-
+    </div>
 
 </div>
