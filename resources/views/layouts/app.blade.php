85a66a953e77732dd770e70077dc4780a73ef878
Abdul Alim <aa.abdulalim13@gmail.com>
2024-08-29 23:34:10 +0600

    feat: :sparkles: added livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Laravel') }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
 </head>
 
 <body>
@@ -14,6 +15,7 @@
         {{ $slot }}
     </main>
     <footer></footer>
+    @livewireScripts
 </body>
 
 </html>
