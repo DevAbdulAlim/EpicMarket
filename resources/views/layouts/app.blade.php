272ea47b78ea31ce9f1ba3464bca817a9e93db13
Abdul Alim <aa.abdulalim13@gmail.com>
2024-08-29 23:23:03 +0600

    feat: :sparkles: added alpinejs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
