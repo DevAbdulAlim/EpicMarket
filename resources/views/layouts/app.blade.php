205d196c3296203dd1e8ae57742bb7b922dab18e
Abdul Alim <aa.abdulalim13@gmail.com>
2024-08-29 23:16:10 +0600

    feat: :sparkles: added tailwind css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Laravel') }}</title>
+    @vite('resources/css/app.css')
 </head>
 
 <body>
