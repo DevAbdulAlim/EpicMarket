c26adcb2d6cd35d1e28882145c35fcea4431ac99
Abdul Alim <aa.abdulalim13@gmail.com>
2024-09-05 21:03:17 +0600

    fix: :bug: sidebar bug

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -2,18 +2,12 @@
     'isOpen' => false,
 ])
 
-<div x-data="{ $isOpen }" x-cloak
-    :class="{
-        'hidden -translate-x-full': !isOpen,
-        'block translate-x-0': isOpen,
-    }"
-    class="relative hidden lg:block transform transition-transform duration-300">
+<div x-data="{ $isOpen: {{ $isOpen ? 'true' : 'false' }}, isCompact: false }" :class="{ '-translate-x-full md:translate-x-0': !isOpen, 'translate-x-0': isOpen }"
+    class="fixed inset-y-0 left-0 transform transition-transform duration-300 bg-gray-800 text-white -translate-x-full md:relative md:translate-x-0">
     <!-- Drawer -->
-    <div
-        class="h-full bg-gray-800 text-white transform transition-transform duration-300 ease-in-out lg:transition-width lg:duration-300">
-
+    <div class="h-full flex flex-col">
         <!-- Drawer Content -->
-        <div x-data="{ isCompact: false }" x-cloak class="flex flex-col space-y-4">
+        <div class="flex-1 flex flex-col space-y-4 p-4">
             <!-- Close Button (Visible only on small screens) -->
             <button @click="isOpen = false" class="lg:hidden text-white mb-4">
                 Close Drawer
@@ -24,10 +18,8 @@
                 Toggle Compact
             </button>
 
-            <div :class="{
-                'lg:w-64': !isCompact,
-                'lg:w-20': isCompact
-            }">
+            <!-- Sidebar Content Area with Compact and Expanded States -->
+            <div class="flex-1 w-64" :class="{ 'w-20': isCompact, 'w-64': !isCompact, }">
                 <!-- Slot for Sidebar or Other Content -->
                 {{ $slot }}
             </div>
