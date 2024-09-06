fa95dfdf12fc9b62cbdd52898257b35779ed1ac9
Abdul Alim <aa.abdulalim13@gmail.com>
2024-09-06 13:31:31 +0600

    refactor: :art: admin topbar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -17,34 +17,7 @@
     <x-sidebar isOpen="isOpen" isCompact="isCompact" />
 
     <!-- Topbar -->
-    <header :class="{ 'md:ml-10': isCompact, 'md:ml-64': !isCompact }"
-        class="sticky top-0 z-10 md:ml-64 flex items-center justify-between transition-all duration-500 ease-in-out bg-white shadow-md p-4">
-        <!-- Hamburger Button for Small Screens -->
-        <button @click="isOpen = !isOpen, isCompact = false" class="text-gray-800 bg-gray-200 rounded-md p-2 md:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
-            </svg>
-        </button>
-
-        <button @click="isCompact = !isCompact"
-            class="text-gray-800 bg-gray-200 rounded-md items-center justify-center h-10 w-10 p-2 hidden md:flex">
-            <i class="fa-solid fa-bars"></i>
-        </button>
-
-        <!-- Topbar Title -->
-        <div class="text-lg font-semibold text-gray-800">{{ config('app.name', 'Laravel') }} Admin</div>
-
-        <!-- Topbar Icons (Profile, Notifications, etc.) -->
-        <div class="flex items-center space-x-4">
-            <button class="text-gray-600 hover:text-gray-800">
-                <i class="fas fa-bell"></i>
-            </button>
-            <button class="text-gray-600 hover:text-gray-800">
-                <i class="fas fa-user-circle"></i>
-            </button>
-        </div>
-    </header>
+    @include('layouts.partial.admin-topbar')
 
     <!-- Main Content Area -->
     <main :class="{ 'md:ml-10': isCompact, 'md:ml-64': !isCompact }"
