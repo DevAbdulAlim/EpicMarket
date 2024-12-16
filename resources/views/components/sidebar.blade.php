41e48fd14e1ca991cb3e3f85091594a3e2ea82cd
Abdul Alim <aa.abdulalim13@gmail.com>
2024-12-16 12:11:59 +0600

    update admin sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -86,6 +86,7 @@
     ];
 @endphp
 
+
 <div x-data="{ isOpen: {{ $isOpen ? 'true' : 'false' }}, $isCompact }" :class="{ '-translate-x-full md:translate-x-0': !isOpen, 'translate-x-0': isOpen }"
     class="fixed inset-y-0 left-0 z-30 transition-all duration-500 ease-in-out -translate-x-full md:translate-x-0">
 
@@ -93,11 +94,11 @@
         <div @click="isOpen = false" class="absolute w-full h-full"></div>
 
         <!-- Sidebar Content Area with Compact and Expanded States -->
-        <div class="h-full overflow-y-auto overflow-x-hidden scrollbar-thin scrollbar-thumb-blue-500 scrollbar-track-gray-200 bg-primary text-white flex-1 w-64 transition-all duration-500 ease-in-out"
+        <div class="h-full overflow-y-auto overflow-x-hidden scrollbar-thin scrollbar-thumb-blue-500 scrollbar-track-gray-200 bg-blue-800 text-white flex-1 w-64 transition-all duration-500 ease-in-out"
             :class="{ 'w-12': isCompact, 'w-64': !isCompact }">
 
             <!-- Sidebar Header -->
-            <div class="sticky top-0 flex items-center bg-primary z-10 border-b justify-between">
+            <div class="sticky top-0 flex items-center bg-blue-800 z-10 border-b justify-between">
                 <div :class="{ 'hidden': isCompact }" class="text-lg p-2 font-bold whitespace-nowrap">Admin Panel</div>
                 <div x-show="isCompact" class="text-lg font-bold p-2 hidden md:block">AP</div>
                 <button @click="isOpen = false"
@@ -111,23 +112,23 @@
             </div>
 
             <!-- Sidebar Navigation Items -->
-            <nav class="flex flex-col mt-4 space-y-2 mb-4">
+            <nav class="flex flex-col mt-4 space-y-2 mb-4 z-10 bg-blue-800">
                 @foreach ($navItems as $item)
                     <!-- If it has no dropdown -->
                     @if (!$item['dropdown'])
-                        <a href="{{ $item['route'] }}"
-                            class="flex items-center p-2 hover:border-l-2 border-primary text-white bg-primary/10">
-                            <i class="{{ $item['icon'] }}"></i>
-                            <span :class="{ 'hidden': isCompact }" class="ml-2">{{ $item['name'] }}</span>
-                        </a>
+                        <div class="z-10 bg-blue-800 p-2"> <a href="{{ $item['route'] }}"
+                                class="flex items-center p-2 rounded-md hover:border-l-2 border-primary hover:bg-blue-700 text-white bg-blue-800/10">
+                                <i class="{{ $item['icon'] }}"></i>
+                                <span :class="{ 'hidden': isCompact }" class="ml-2">{{ $item['name'] }}</span>
+                            </a></div>
                     @else
                         <!-- Dropdown Section -->
                         {{-- compact --}}
-                        <div class="z-10" :class="{ 'hidden': !isCompact }"
+                        <div class="z-10 p-2" :class="{ 'hidden': !isCompact }"
                             @mouseenter="activeDropdown === '{{ strtolower($item['name']) }}' ? activeDropdown = null : activeDropdown = '{{ strtolower($item['name']) }}'"
                             @mouseleave="activeDropdown === '{{ strtolower($item['name']) }}' ? activeDropdown = null : activeDropdown = '{{ strtolower($item['name']) }}'">
-                            <div
-                                class="flex items-center p-2 hover:border-l-2 border-primary text-white cursor-pointer">
+                            <div class="flex items-center p-2 rounded-md hover:border-l-2 border-primary text-white cursor-pointer"
+                                :class="{ 'bg-blue-700': activeDropdown === '{{ strtolower($item['name']) }}' }">
                                 <a href="{{ $item['route'] }}">
                                     <i class="{{ $item['icon'] }} text-xl"></i>
                                     <svg :class="{ 'rotate-180': activeDropdown === '{{ strtolower($item['name']) }}', 'hidden': isCompact }"
@@ -142,11 +143,10 @@
                             <!-- Dropdown Items -->
                             <ul x-show="activeDropdown === '{{ strtolower($item['name']) }}'" x-collapse
                                 class="ml-8 z-10"
-                                :class="{ 'absolute left-2 z-50 rounded-md bg-white w-48 shadow-2xl -mt-10': isCompact }">
+                                :class="{ 'absolute left-2 z-50 rounded-md overflow-hidden bg-white w-48 shadow-2xl -mt-10': isCompact }">
                                 @foreach ($item['items'] as $subItem)
-                                    <li
-                                        :class="{ ' px-4 py-2 text-blue-800 hover:bg-blue-500 hover:text-white': isCompact }">
-                                        <a href="{{ $subItem['route'] }}" @click.stop>
+                                    <li :class="{ ' text-blue-800 hover:bg-blue-500 hover:text-white': isCompact }">
+                                        <a class="block px-4 py-2" href="{{ $subItem['route'] }}" @click.stop>
                                             {{ $subItem['name'] }}
                                         </a>
                                     </li>
@@ -154,14 +154,17 @@
                             </ul>
                         </div>
                         {{-- non-compact --}}
-                        <div class="z-10" :class="{ 'hidden': isCompact }">
-                            <div
-                                class="flex items-center p-2 hover:border-l-2 border-primary text-white cursor-pointer">
-                                <a href="{{ $item['route'] }}" class="whitespace-nowrap">
-                                    <i class="{{ $item['icon'] }}" :class="{ 'hidden': isCompact }"></i>
-                                    <span class="ml-2">{{ $item['name'] }}</span></a>
-                                <svg @click="activeDropdown === '{{ strtolower($item['name']) }}' ? activeDropdown = null : activeDropdown = '{{ strtolower($item['name']) }}'"
-                                    :class="{ 'rotate-180': activeDropdown === '{{ strtolower($item['name']) }}', 'hidden': isCompact }"
+                        <div class="z-10 p-2" :class="{ 'hidden': isCompact }">
+                            {{-- <a href="{{ $item['route'] }}" class="whitespace-nowrap"> --}}
+                            <div @click="activeDropdown === '{{ strtolower($item['name']) }}' ? activeDropdown = null : activeDropdown = '{{ strtolower($item['name']) }}'"
+                                :class="{
+                                    'flex items-center p-2 hover:border-l-2 border-primary hover:bg-blue-700 rounded-md text-white cursor-pointer': true,
+                                    'bg-blue-700': activeDropdown === '{{ strtolower($item['name']) }}'
+                                }">
+
+                                <i class="{{ $item['icon'] }}" :class="{ 'hidden': isCompact }"></i>
+                                <span class="ml-2">{{ $item['name'] }}</span>
+                                <svg :class="{ 'rotate-180': activeDropdown === '{{ strtolower($item['name']) }}', 'hidden': isCompact }"
                                     xmlns="http://www.w3.org/2000/svg"
                                     class="ml-auto h-5 w-5 transition-transform transform" fill="none"
                                     viewBox="0 0 24 24" stroke="currentColor">
@@ -169,15 +172,15 @@
                                         d="M19 9l-7 7-7-7" />
                                 </svg>
                             </div>
+                            {{-- </a> --}}
 
                             <!-- Dropdown Items -->
                             <ul x-show="activeDropdown === '{{ strtolower($item['name']) }}'" x-collapse
-                                class="ml-7 z-10 space-y-1"
-                                :class="{ 'absolute left-2 p-2 z-50 bg-white w-fit shadow-2xl': isCompact }">
+                                class="ml-7 z-10 space-y-1 bg-blue-800 border-l"
+                                :class="{ 'absolute z-50 bg-white w-fit shadow-2xl': isCompact }">
                                 @foreach ($item['items'] as $subItem)
-                                    <li>
-                                        <a href="{{ $subItem['route'] }}"
-                                            :class="{ 'text-blue-800 hover:text-blue-500': isCompact }" @click.stop>
+                                    <li class="hover:bg-blue-500">
+                                        <a href="{{ $subItem['route'] }}" class="block p-2">
                                             {{ $subItem['name'] }}
                                         </a>
                                     </li>
